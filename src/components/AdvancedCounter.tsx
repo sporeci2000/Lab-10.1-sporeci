4ac0c95c9172d086911ef5e961e060e4136e1dce
Sintja <sporeci2000@gmail.com>
2025-08-03 20:17:41 -0400

    Count History

--- src/components/AdvancedCounter.tsx
+++ src/components/AdvancedCounter.tsx
@@ -1,11 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const AdvancedCounter = () => {
     //State for count
     const [count, setCount] = useState<number>(0); //Count initialized at 0
+    const [track, setTrack] = useState<number[]>([]); //past values
 
     const increment = () => setCount(count + 1);
     const decrement = () => setCount(count - 1);
+
+    //Tracking everything
+    //useEffect runs after every render when count changes
+    useEffect(() => {
+        setTrack((prev) => [...prev, count]);
+    }, [count]); 
 
     return (
         <div>
@@ -14,6 +21,11 @@ const AdvancedCounter = () => {
 
             <button onClick = {increment}> Increment </button>
             <button onClick = {decrement}> Increment </button>
+
+            <div>
+                <h4> Count History: </h4>
+                <p>{track.join(", ")}</p>
+            </div>
         </div>
     );
 };
